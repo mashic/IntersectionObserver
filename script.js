eb3b6803f2186a3c49cda28c5edf98addc252d83
RSSdoo <masic.ramiz.7@gmail.com>
2022-01-20 16:39:11 +0100

    copilot comment

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
-const cards = window.document.querySelectorAll('.card');
-const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
+const cards = window.document.querySelectorAll('.card'); // Select all cards
+/*
+IntersectionObserver is a class, so we need to create an instance of it, 
+and pass it an array of callbacks, which will be called when the intersection of the target element with the viewport changes.
+*/
+const observer = new IntersectionObserver(entries => { 
+        entries.forEach(entry => { // For each entry
                 // console.log(entry);
-                entry.target.classList.toggle('show', entry.isIntersecting);
+                entry.target.classList.toggle('show', entry.isIntersecting); // toggle show class
             }
         );
     }, {
-        rootMargin: '0px -200px -100px 0px'
+        rootMargin: '0px -200px -100px 0px' // Margin around the viewport
     }, {
-        threshold: 1
+        threshold: 1 
+        /*
+        Threshold of 1 means that the intersection ratio must be at least 1, 
+        so the element is at least 100% visible, and not just 50%, for example.
+        */
     }
 
 );
